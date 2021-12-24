ac54ce8ad8e09df9eeb04690b3ce0f58591a2477
jusski <jusski@sdf-eu.org>
2021-12-24 21:28:13 +0200

    implemented writing to file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,11 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
@@ -6,13 +13,16 @@ import java.util.stream.Collectors;
 
 public class Main
 {
-	public static void printEntries(File entry, String prefix)
+	private static BufferedWriter fileWriter;
+	
+	public static void printEntries(File entry, String prefix) throws IOException
 	{
-		List<File> files = Arrays.asList(entry.listFiles()).stream().filter(File::isFile).sorted()
-				.collect(Collectors.toList());
+		List<File> files = Arrays.asList(entry.listFiles()).stream().filter(File::isFile).filter(e -> !e.isHidden())
+				.sorted().collect(Collectors.toList());
 
 		List<File> directories = Arrays.asList(entry.listFiles()).stream().filter(File::isDirectory)
-				.filter(e -> !e.getName().startsWith(".git")).sorted().collect(Collectors.toList());
+				.filter(e -> !e.isHidden()).sorted().collect(Collectors.toList());
+
 		if (directories.size() == 0)
 		{
 			printFiles(files, prefix);
@@ -22,65 +32,61 @@ public class Main
 			printFiles(files, prefix + "  │");
 		}
 
-		if (directories.size() > 1)
-		{
-			printDirectories(directories, prefix + "  ");
+		printDirectories(directories, prefix + "  ");
 
-		}
-		else
-		{
-			printDirectory(directories, prefix + "  ");
-		}
 	}
 
-	public static void printFiles(List<File> files, String prefix)
+	public static void printFiles(List<File> files, String prefix) throws IOException
 	{
 		String fileNamePrefix = "    ";
-		Iterator<File> iterator = files.iterator();
-		if (files.size() > 0)
+		if (!files.isEmpty())
 		{
-			for (int fileIndex = 0; fileIndex < files.size(); ++fileIndex)
+			for (File file : files)
 			{
-				File file = iterator.next();
 				System.out.println(prefix + fileNamePrefix + file.getName());
-
+				fileWriter.write(prefix + fileNamePrefix + file.getName());fileWriter.newLine();
 			}
-			System.out.println(prefix);
+			System.out.println(prefix); 
+			fileWriter.write(prefix);fileWriter.newLine();
 		}
 
 	}
 
-	public static void printDirectories(List<File> directories, String prefix)
+	public static void printDirectories(List<File> directories, String prefix) throws IOException
 	{
 		String directoryNamePrefix = "├───";
 		Iterator<File> iterator = directories.iterator();
-		for (int fileIndex = 0; fileIndex < directories.size() - 1; ++fileIndex)
+		if (iterator.hasNext())
 		{
+			for (int i = 0; i < directories.size() - 1; ++i)
+			{
+				File directory = iterator.next();
+				System.out.println(prefix + directoryNamePrefix + directory.getName());
+				fileWriter.write(prefix + directoryNamePrefix + directory.getName());fileWriter.newLine();
+				printEntries(directory, prefix + "│" + " ");
+			}
 			File directory = iterator.next();
-			System.out.println(prefix + directoryNamePrefix + directory.getName());
-			printEntries(directory, prefix + "│" + "  ");
+			printLastDirectory(directory, prefix);
 		}
-		File directory = iterator.next();
-		System.out.println(prefix + "└───" + directory.getName());
+	}
+
+	public static void printLastDirectory(File directory, String prefix) throws IOException
+	{
+		String directoryNamePrefix = "└───";
+		System.out.println(prefix + directoryNamePrefix + directory.getName());
+		fileWriter.write(prefix + directoryNamePrefix + directory.getName());fileWriter.newLine();
 		printEntries(directory, prefix + "  ");
 	}
 
-	public static void printDirectory(List<File> directories, String prefix)
+	public static void main(String[] args) throws IOException
 	{
-		String directoryNamePrefix = "└───";
-		for (File directory : directories)
-		{
-			System.out.println(prefix + directoryNamePrefix + directory.getName());
-			printEntries(directory, prefix + "  ");
-		}
-	}
-
-	public static void main(String[] args)
-	{
-		String path = "C:/epam/calculator".replace('/', File.separatorChar);
+		String path = "C:/epam/exceptions".replace('/', File.separatorChar);
 		File file = new File(path);
-
+		fileWriter = Files.newBufferedWriter(Paths.get("ky123.txt"), StandardCharsets.UTF_16, StandardOpenOption.CREATE, StandardOpenOption.WRITE);
 		printEntries(file, "");
+		fileWriter.flush();
+		fileWriter.close();
+	
 
 	}
 
